e27cdc41856fb613f3a113745f52641810d6c43a
ericklrobles <eglrobles@gmail.com>
2023-07-03 21:24:49 -0700

    commit1

---
+++ Variables/exercise1.cpp
@@ -0,0 +1,13 @@
+// Create a program that calculates the area and perimeter of a rectangle using user-provided length and width values.
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+   
+    return 0;
+}
+
