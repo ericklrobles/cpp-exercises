22353866dfc7248a88efae812b08ceeca4a8ee82
ericklrobles <eglrobles@gmail.com>
2023-07-03 21:38:11 -0700

    commit2

--- Variables/exercise1.cpp
+++ Variables/exercise1.cpp
@@ -2,11 +2,18 @@
 
 #include <iostream>
 using namespace std;
+int length = 23;
+int width = 5;
+
 
 
 
 int main()
 {
+    cout <<"Area of a rectangle: " <<length * width << endl;
+    cout <<"Perimeter of a rectangle: "<< 2*(length + width) << endl;
+
+
    
     return 0;
 }
