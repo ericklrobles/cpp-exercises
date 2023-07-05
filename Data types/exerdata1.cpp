26fa94961f3dcf80c8f8ad013cf97bbc1eab76e8
ericklrobles <eglrobles@gmail.com>
2023-07-04 19:08:31 -0700

    dataexer1

---
+++ Data types/exerdata1.cpp
@@ -0,0 +1,14 @@
+// Develop a program that stores and displays information about a student, including their name, age, and grade.
+#include <iostream>
+using namespace std;
+
+string student_Name = "Erick Davis";
+int student_age = 18;
+int student_grade = 12;
+
+
+int main()
+{
+    cout <<"Student: "<< student_Name << " Age: " << student_age << " Grade: "<< student_grade << endl;
+    return 0;
+}
