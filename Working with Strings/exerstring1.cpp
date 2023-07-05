dd2a2e3b35a117588a4d7b7e3d999f753d654062
ericklrobles <eglrobles@gmail.com>
2023-07-04 20:30:28 -0700

    newexer

---
+++ Working with Strings/exerstring1.cpp
@@ -0,0 +1,9 @@
+//Build a program that checks if a given word is a palindrome (reads the same forwards and backward).
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string word = "Mom";
+    return 0;
+}
