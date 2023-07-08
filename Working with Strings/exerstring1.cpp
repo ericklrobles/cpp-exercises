edbaf9a3541acd31b858887b877c4dc78fccc7ac
ericklrobles <eglrobles@gmail.com>
2023-07-08 13:39:25 -0700

    reversestringcommit

--- Working with Strings/exerstring1.cpp
+++ Working with Strings/exerstring1.cpp
@@ -1,9 +1,13 @@
 //Build a program that checks if a given word is a palindrome (reads the same forwards and backward).
 #include <iostream>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-    string word = "Mom";
+    string word = "radar";
+    reverse(word.begin(), word.end());
+    cout <<word;
     return 0;
 }
