0359c92c9aa51faf9b7bbcc4723fb7a1163e8467
ericklrobles <eglrobles@gmail.com>
2023-07-09 15:04:38 -0700

    finalprodcomments

--- Arrays/exerarray1.cpp
+++ Arrays/exerarray1.cpp
@@ -6,8 +6,9 @@ using namespace std;
 int main()
 {
     int num[5] = {5,4,32,6,9};
-    int size = sizeof(num) / sizeof(num[0]);
-    int *maxElement = max_element(num, num + size);
+    int size = sizeof(num) / sizeof(num[0]); // finding out the size of the array to use the functions below
+    // functions used to find max & min numbers in the following array
+    int *maxElement = max_element(num, num + size); // using built in function algorithm library
     int *minElement = min_element(num, num + size);
     cout << "Largest Number: " << *maxElement << endl;
     cout << "Smallest Number: " << *minElement << endl;
