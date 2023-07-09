449d46fcb63d6f0fe44e5156c0a55c848f0d838a
ericklrobles <eglrobles@gmail.com>
2023-07-09 13:57:29 -0700

    working with nums

--- Working with Numbers/exernum2.cpp
+++ Working with Numbers/exernum2.cpp
@@ -1,23 +1,28 @@
 //Create a program that determines whether a given number is prime or not.
 #include <iostream>
 using namespace std;
-bool isPrime(int num);
+
 
 int main()
 {
 
     int num;
-    bool flag;
+    bool isPrime = true;
     cout << "Enter a number to determine whether it is prime or not (pos integer)" << endl;
     cin >> num;
-    if(flag == true){
-        
+    for(int i = 2; i <= num/2; i++){
+    if (num % i == 0){
+        isPrime = false;
+        break;
     }
-    
+    }
 
 
-
-
+    if(isPrime == true){
+        cout << "The number is a prime number";
+    }else{
+        cout <<"The number is not a prime number";
+    }
 
 
     return 0;
