dd0c8cb1677a846589123affe76fd7f3da449560
ericklrobles <eglrobles@gmail.com>
2023-07-09 14:23:30 -0700

    basicprompt

---
+++ Getting User Input/userinputexer1.cpp
@@ -0,0 +1,17 @@
+// Write a program that prompts the user for their name and greets them with a personalized message.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string input;
+    cout << "Hello what is your name lad!"<< endl;
+    cin >> input; 
+    cout << "Hello, " << input << endl;
+
+
+
+
+
+    return 0;
+}
