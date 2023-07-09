bffabb415b00789f64ed6bd0be67144f1c3f21c0
ericklrobles <eglrobles@gmail.com>
2023-07-09 14:10:17 -0700

    error handling/fixes if input = 1|0

--- Working with Numbers/exernum2.cpp
+++ Working with Numbers/exernum2.cpp
@@ -1,7 +1,7 @@
 //Create a program that determines whether a given number is prime or not.
 #include <iostream>
 using namespace std;
-
+// prime number has no remainders & can be multiplied once
 
 int main()
 {
@@ -10,9 +10,15 @@ int main()
     bool isPrime = true;
     cout << "Enter a number to determine whether it is prime or not (pos integer)" << endl;
     cin >> num;
-    for(int i = 2; i <= num/2; i++){
-    if (num % i == 0){
+
+    if (num == 0 || num == 1){
         isPrime = false;
+    }
+
+    // to check if the user input given is a prime number 
+    for(int i = 2; i <= num/2; i++){ // for loop 
+    if (num % i == 0){ // modulas operator finding remainders
+        isPrime = false;// changes the bool value to false only if the condition occurs
         break;
     }
     }
