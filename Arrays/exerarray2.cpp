7239452e4e4f87b1a4fc4f236bc0ce963c4fa6dc
ericklrobles <eglrobles@gmail.com>
2023-07-10 15:30:58 -0700

    arraysetproblem#2

--- Arrays/exerarray2.cpp
+++ Arrays/exerarray2.cpp
@@ -1,13 +1,18 @@
 // Create a program that reverses the elements in an array.
 #include <iostream>
 #include <algorithm>
+#include <iterator>
 using namespace std;
 
 int main()
 {
-  int array [6] = {1,2,3,4,5,6};
-  
-
+   const int size = 6;
+   int array [size] = {1,2,3,4,5,6};
+   reverse(begin(array),end(array));
+   
+   for(auto item:array){ // The auto keyword in C++ automatically detects and assigns a data type to the variable with which it is used. T
+    cout << item << endl;
+   }
 
     return 0;
 }
