e1f31b8826ec4cb7db1d3662f10554cc740461ed
ericklrobles <eglrobles@gmail.com>
2023-07-10 16:19:32 -0700

    finishedprod of funcexer2

--- Functions/funcexer2.cpp
+++ Functions/funcexer2.cpp
@@ -7,10 +7,12 @@ int main()
     int givenNum, result;
     cout << "Enter a number to determine whether its odd or even" << endl;
     cin >> givenNum;
-
-
-
-
+    result = givenNum % 2; // using modulus operator and using dividor of 2 to find any variables 
+    if(result > 0){
+        cout << givenNum << " is a odd number!" << endl;// odd number = remainder > 0
+    }else{
+        cout << givenNum << " is a even number!" << endl;// even number = no remainders
+    }
 
     return 0;
 }
