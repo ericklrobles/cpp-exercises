aa81186e138c61712b72734d131140e6cd24c748
ericklrobles <eglrobles@gmail.com>
2023-07-10 16:39:35 -0700

    layout of exer2if

---
+++ if statements/ifexer2.cpp
@@ -0,0 +1,10 @@
+// Create a program that checks if a user-provided number is positive, negative, or zero using if-else statements.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+    return 0;
+}
