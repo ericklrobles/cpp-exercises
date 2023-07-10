7c9e4a729fa545b59fe359594c745966d48ede58
ericklrobles <eglrobles@gmail.com>
2023-07-10 15:10:51 -0700

    exer2begin

---
+++ Arrays/exerarray2.cpp
@@ -0,0 +1,13 @@
+// Create a program that reverses the elements in an array.
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+  int array [6] = {1,2,3,4,5,6};
+  
+
+
+    return 0;
+}
