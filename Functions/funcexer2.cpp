a97d7de731ca1c52d075f33db50373a33d5d2453
ericklrobles <eglrobles@gmail.com>
2023-07-10 16:10:09 -0700

    layout

---
+++ Functions/funcexer2.cpp
@@ -0,0 +1,16 @@
+// Develop a program that determines if a given number is even or odd using a function.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int givenNum, result;
+    cout << "Enter a number to determine whether its odd or even" << endl;
+    cin >> givenNum;
+
+
+
+
+
+    return 0;
+}
