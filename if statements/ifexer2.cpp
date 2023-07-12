21b63907e5514142aab6988446984f1c5274687f
ericklrobles <eglrobles@gmail.com>
2023-07-12 16:58:55 -0700

    exer2ifstatementsfinalprod

--- if statements/ifexer2.cpp
+++ if statements/ifexer2.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 int main()
 {
+    int givenNum;
+    cout << "Enter any number (neg,pos,zero)" << endl;
+    cin >> givenNum;
 
+    if (givenNum == 0){
+        cout<< "Number is Zero" << endl;
+    }
+    if (givenNum > 0){
+        cout <<"Number is a positive number"<< endl;
+    }else if(givenNum < 0){
+        cout <<"Number is a negative number" << endl;
+    }
 
     return 0;
 }
