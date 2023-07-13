984e4890ca71d704c529056b0a888beef9dca406
ericklrobles <eglrobles@gmail.com>
2023-07-12 18:20:50 -0700

    comments

--- Loops/loopsexer2.cpp
+++ Loops/loopsexer2.cpp
@@ -12,7 +12,7 @@ int main()
     //loop to find the sum of all numbers from 1 to the given number
 
     while (givenNum > 0){
-        sum += givenNum; // equivalent to sum = sum + givenNum 
+        sum += givenNum; // equivalent to sum = sum + givenNum basically just adding the current value to sum
         givenNum -= 1; //decreasing value of integer by 1
     }
     cout << "The sum of all numbers from 1 to " << originalNum << " is " << sum << endl;
