322ebea18081650ad57f318182277a5117ac5d8a
ericklrobles <eglrobles@gmail.com>
2023-07-13 16:26:39 -0700

    layout

---
+++ Classes & Objects/class2.exer.cpp
@@ -0,0 +1,23 @@
+//Build a program that represents a basic game character using a class. Include attributes like name, health, and abilities, along with functions for character actions.
+#include <iostream>
+using namespace std;
+
+class game{
+    string name;
+    int health;
+    string abilities;
+    
+
+};
+
+
+
+
+int main()
+{
+    
+
+
+
+    return 0;
+}
