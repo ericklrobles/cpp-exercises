a382fa1f1b7a66d45b3ca2ad15d73cbd2c4f8cdc
ericklrobles <eglrobles@gmail.com>
2023-08-07 19:33:55 -0700

    cpp function problem set resolved/fixed

--- Functions/funcexer2.cpp
+++ Functions/funcexer2.cpp
@@ -1,17 +1,19 @@
 // Develop a program that determines if a given number is even or odd using a function.
 #include <iostream>
-using namespace std;
-
+#include <string>
+bool isEven(int givenNum){
+    return givenNum % 2 == 0;
+}
 int main()
 {
-    int givenNum, result;
-    cout << "Enter a number to determine whether its odd or even" << endl;
-    cin >> givenNum;
-    result = givenNum % 2; // using modulus operator and using dividor of 2 to find any variables 
-    if(result > 0){
-        cout << givenNum << " is a odd number!" << endl;// odd number = remainder > 0
+    int givenNum;
+    std::cout << "Enter a number to determine whether its odd or even\n";
+    std::cin >> givenNum;
+    isEven(givenNum);// using function to determine odd or even as shown above
+    if(isEven(givenNum)){
+        std::cout << givenNum << " is a even number\n";// odd number = remainder > 0
     }else{
-        cout << givenNum << " is a even number!" << endl;// even number = no remainders
+        std::cout << givenNum << " is a odd number!\n";// even number = no remainders
     }
 
     return 0;
